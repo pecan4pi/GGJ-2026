2e5ccf6cc4faaff6a557e1a10aa445817385396e
pecan4pi <157906054+pecan4pi@users.noreply.github.com>
2026-01-31 20:51:39 -0500

    started the actual coding, going to make the custom animation playing system

--- main.ts
+++ main.ts
@@ -1,37 +1,43 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (!(topdown)) {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy += -250
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (topdown) {
-        topdown = false
-    } else {
-        topdown = true
-    }
-})
-let topdown = false
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
-mySprite = sprites.create(img`
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 
-    `, SpriteKind.Player)
+mySprite = sprites.create(assets.image`Main_Character_Base`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
-topdown = false
+mySprite.ay = 500
+controller.moveSprite(mySprite, 100, 0)
 game.onUpdate(function () {
-    if (topdown) {
-        controller.moveSprite(mySprite, 100, 100)
-        mySprite.ay = 0
-        mySprite.setVelocity(0, 0)
-    } else if (!(topdown)) {
-        mySprite.ay = 500
-        controller.moveSprite(mySprite, 100, 0)
+    if (mySprite.vx < 0 && mySprite.vy < 0) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        false
+        )
     }
 })
